48a40d7a3920e714e3e564ccb359214be73795d9
Patrick Shuff <patrick.shuff@gmail.com>
2016-11-28 20:50:13 -0800

    Cleaning up some lint.  Dropping port from output

--- hdhomerun.go
+++ hdhomerun.go
@@ -3,6 +3,7 @@ package main
 import "bytes"
 import "fmt"
 import "log"
+import "strings"
 import "net"
 
 var source_port int = 4321
@@ -22,56 +23,34 @@ func main() {
 }
 
 func sendUDPProbes() {
-	const discovery_bin = "\x00\x02\x00\x0c\x01\x04\x00\x00\x00\x01\x02\x04\xff\xff\xff\xff\x4e\x50\x7f\x35"
-
+	// The discovery binary was discovered via tcpdump 
+	const discovery_bin = (
+		"\x00\x02\x00\x0c\x01\x04\x00\x00\x00\x01\x02\x04\xff\xff" +
+			"\xff\xff\x4e\x50\x7f\x35")
 
 	// Setup socket that is going to send/receive discovery datagrams
 	RAddr,_ := net.ResolveUDPAddr("udp","192.168.174.255:65001")
 	ServerAddr, _ := net.ResolveUDPAddr("udp","192.168.174.168:4322")
 	listen_conn, _ := net.ListenUDP("udp", ServerAddr)
 
-	// fmt.Println("Listening on: ", listen_conn.LocalAddr())
-	
 	listen_conn.WriteTo([]byte(discovery_bin), RAddr)
 
 	// Listen for a response
 	buf := make([]byte, 1024)
 	for {
 		_,addr,err := listen_conn.ReadFromUDP(buf)
-		// msg := "hdhomerun device 1322F2F9 found at 192.168.174.249"
-		msg := "hdhomerun device %x found at %s"
-		// fmt.Println("Received ",string(buf[40:n]), " from ", addr)
-		fmt.Printf(msg, buf[12:16], addr)
+
+		msg := "hdhomerun device %x found at %s\n"
+		// e.g. "hdhomerun device 1322F2F9 found at 192.168.174.249"
+		// 
+		hdhr_ip := strings.Split(addr.String(), ":")[0]
+		fmt.Printf(msg, buf[12:16], hdhr_ip)
 		
 		if err != nil {
 			fmt.Println("Error: ",err)
 		}
 		break
 	}
-}
-
-func startUDPServer() {
-	fmt.Println("Starting listening server probes...")
-	ServerAddr, err := net.ResolveUDPAddr("udp","127.255.255.255:4321")
-
-	conn, err := net.ListenUDP("udp", ServerAddr)
-	if err != nil {
-		fmt.Println("ERROR")
-		return
-	}
-
-	buf := make([]byte, 1024)
-
-
-	for {
-		n,addr,err := conn.ReadFromUDP(buf)
-		fmt.Println("Received ",string(buf[0:n]), " from ",addr)
-		
-		if err != nil {
-			fmt.Println("Error: ",err)
-		} 
-	}
-
 }
 
 func getChannels() {
